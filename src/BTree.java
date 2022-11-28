f0d83b94f0a49c4691a4331f35672aa1f7015000
lyh7629 <tom19981122@gmail.com>
2022-11-29 03:37:57 +0800

    Start

--- src/BTree.java
+++ src/BTree.java
@@ -1,23 +1,67 @@
 import java.util.List;
 
 public class BTree {
+    private Node root = null;
 
-    private interface Node {}
+    private interface Node {
+        double fillFactor();
+    }
 
     private class IndexNode implements Node {
         String[] keys;
         Node[] childNodes;
+
+        LeafNode find(String key, Node node) {
+            if (node instanceof LeafNode)
+                return ((LeafNode) node).find(key);
+
+            for (int i = 0; i < keys.length; i++) {
+                if (key.compareTo(keys[i]) < 0)
+                    return find(key, childNodes[i]);
+                else if (key.compareTo(keys[i]) == 0 || i == keys.length - 1)
+                    return find(key, childNodes[i + 1]);
+            }
+            throw new KeyNotFoundException(key);
+        }
+
+        @Override
+        public double fillFactor() {
+            int total = 0;
+            for (String k : keys) {
+                if (k != null) total += 1;
+            }
+            return total / keys.length;
+        }
     }
 
     private class LeafNode implements Node {
         Record[] records;
         LeafNode leftLeafNode;
         LeafNode rightLeafNode;
+
+        LeafNode find(String key) {
+            for (Record r : records)
+                if (r.equals(key)) return this;
+            throw new KeyNotFoundException(key);
+        }
+
+        @Override
+        public double fillFactor() {
+            int total = 0;
+            for (Record r : records) {
+                if (r != null) total += 1;
+            }
+            return total / records.length;
+        }
     }
 
     private class Record {
         String key;
         int rid = 0;
+
+        public boolean equals(String key) {
+            return this.key.equals(key);
+        }
     }
 
     public BTree(String filename) {
@@ -64,13 +108,59 @@ public class BTree {
     }
 
     public void delete(String key) {
+        if (root == null) return;
+        delete(key, root);
+    }
+
+    private void delete(String key, Node node) {
+        if (node instanceof LeafNode) {
+            LeafNode ln = (LeafNode) node;
+            int pos = 0;
+
+            // Find the record and delete
+            while (pos < ln.records.length) {
+                if (ln.records[pos].equals(key)) {
+                    ln.records[pos] = null;
+                    break;
+                } else if (pos == ln.records.length - 1)
+                    throw new KeyNotFoundException(key);
+                pos++;
+            }
+
+            // Shift the record position
+            while (pos < ln.records.length - 1) {
+                ln.records[pos] = ln.records[pos - 1];
+            }
+
+            ln.records[ln.records.length - 1] = null;
+            return;
+        }
+
+        IndexNode in = (IndexNode) node;
+
+        for (int i = 0; i < in.keys.length; i++) {
+            if (key.compareTo(in.keys[i]) < 0)
+                delete(key, in.childNodes[i]);
+            else if (key.compareTo(in.keys[i]) == 0 || i == in.keys.length - 1)
+                delete(key, in.childNodes[i + 1]);
+        }
+
         // TODO
+        // Check fill factor
     }
 
     public List<String> search(String key1, String key2) {
         // TODO
         return null;
     }
+
+//    private Node findParent(Node node) {
+//
+//    }
+
+//    private Node find(String key, Node node) {
+//
+//    }
 
     public void dumpStatistics() {
         // TODO
@@ -82,6 +172,12 @@ public class BTree {
 
     public void printNode() {
         // TODO
+    }
+
+    public static class KeyNotFoundException extends RuntimeException {
+        public KeyNotFoundException(String key) {
+            super(key + " is not found in the tree.");
+        }
     }
 
     public static void main(String[] args) {
